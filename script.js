e156bc636a0d94d0185f8bcbc7f47dd9cbd86ab4
Gregory Brunner <gbrunner@esri.com>
2020-12-22 10:45:35 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var map = L.map('map').setView([41.8781, -87.6298], 10);
+
+  // load a tile layer
+  L.tileLayer('https://a.tile.stamen.com/toner/{z}/{x}/{y}.png',
+    {
+      attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
+      maxZoom: 17
+    }).addTo(map);
+
+  // load GeoJSON from an external file
+  $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-python-for-gis-and-rs/master/Week%201/chicago.geojson",function(data){
+    // add GeoJSON layer to the map once the file is loaded
+    L.geoJson(data).addTo(map);
+  });
