59ae7bf05e0903582d725083eb4702219d9becc1
Gregory Brunner <gbrunner@esri.com>
2021-06-10 12:53:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var map = L.map('map').setView([41.8781, -87.6298], 10);
     }).addTo(map);
 
   // load GeoJSON from an external file
-  $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-python-for-gis-and-rs/master/Week%201/chicago.geojson",function(data){
+  $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/chicago.geojson",function(data){
     // add GeoJSON layer to the map once the file is loaded
     L.geoJson(data).addTo(map);
   });
