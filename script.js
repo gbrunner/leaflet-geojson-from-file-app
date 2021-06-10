7754ba8a51f8248eb5c11a5930324e0375c3a864
Gregory Brunner <gbrunner@esri.com>
2021-06-10 12:55:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var map = L.map('map').setView([41.8781, -87.6298], 10);
 
   // load a tile layer
-  L.tileLayer('http://a.tile.stamen.com/toner/{z}/{x}/{y}.png',
-    {
-      attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
-      maxZoom: 17
-    }).addTo(map);
+ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+}).addTo(map);
 
   // load GeoJSON from an external file
   $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/chicago.geojson",function(data){
